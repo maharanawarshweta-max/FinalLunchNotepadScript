410137a34132f7778a144e0a71e7808a1fc232c1
maharanawarshweta-max <maharanawarshweta@gmail.com>
2025-08-07 17:15:47 +0530

    Add files via upload

--- Launch_Notepad.bat
+++ Launch_Notepad.bat
@@ -1,6 +1,8 @@
-@echo off
-echo Launching Google Chrome...
-start "" "C:\Program Files\Notepad++\notepad++.exe"
-echo Chrome is starting...
-timeout /t 3 >nul
-exit
+@echo off 
+
+echo launching First application > C:\temp\testlaunchNotepad.log
+echo Parameter Provided for Launch Script : %1 >> C:\temp\testlaunchNotepad.log
+
+C:\tcplm\AdientToolbar\BasePackage\Notepad_DCT\FinalLunchNotepadpacakge-main\notepad++.exe
+
+echo Notpad launched >> C:\temp\testlaunchNotepad.log

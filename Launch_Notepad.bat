972d9f6e8b9f55a9c87531ceec478c88ddf5da69
maharanawarshweta-max <maharanawarshweta@gmail.com>
2025-08-07 17:20:50 +0530

    Update Launch_Notepad.bat

--- Launch_Notepad.bat
+++ Launch_Notepad.bat
@@ -5,4 +5,5 @@ echo Parameter Provided for Launch Script : %1 >> C:\temp\testlaunchNotepad.log
 
 C:\tcplm\AdientToolbar\BasePackage\Notepad_DCT\FinalLunchNotepadpacakge-main\notepad++.exe
 
+
 echo Notpad launched >> C:\temp\testlaunchNotepad.log
